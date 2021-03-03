6340ec40ba7a2c6b785859c1047c0a59b133cbd0
Matthew Carlson <matthew.carlson@gmail.com>
2021-03-02 20:34:27 -0600

    Updated html to test github push action

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          First change to test github actions
         </p>
         <a
           className="App-link"
